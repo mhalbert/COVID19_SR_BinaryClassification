2f290a2deeef0b56c60af3f268fd1a213f270d2a
elegs99 <71243839+elegs99@users.noreply.github.com>
2023-05-02 16:16:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def data_constructor(filepath, classes , dim_size ,index  ,bboxes , interpolatio
         x1,y1,x2,y2 = bboxes[i]
         img = img[y1:y2,x1:x2]
         img64 = cv2.resize(img, dim_size , interpolation = interpolation)
-        img128 = cv2.resize(img, (128,128), interpolation = interpolation)
+        img128 = cv2.resize(img, (256,256), interpolation = interpolation)
         filename, _ = os.path.splitext(os.path.basename(filepath[i]))
 
         if y[label_index] == 0:
